cd99ce9cbaa89572c7d7f35a465d90e5aed78723
João Lamaison <90867981+lamaison22@users.noreply.github.com>
2024-01-30 14:29:58 -0300

    a

--- fullstack/server/routes/propertyRoute.js
+++ fullstack/server/routes/propertyRoute.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import { createProperty, deleteProperty, editProperty, getAllProperties, getProperty } from '../controllers/propertyController.js'
 import jwtCheck from '../config/auth0Config.js'
-import { updateResidency } from '../../client/src/utils/api.js'
 const router = express.Router()
 
 router.post("/create", jwtCheck ,createProperty)
